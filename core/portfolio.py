ad3ea9461e2ac981eb16d07dfb46c5c1b040ce57
Michael Gesualdo <mgesualdo19@gmail.com>
2026-07-24 15:47:01 -0400

    Updated Portfolio & Thesis Doc

---
+++ core/portfolio.py
@@ -0,0 +1,73 @@
+# The following links contain the etfs to be included in this portfolio exercise segmented by asset class and deeper level segmentation.
+
+# Fixed Income:
+
+# Long matruity and duration (non-FRN) 15+ year maturity
+# US Treasuries (pick only 1 of the following either hedged or not hedged)
+# 1) iShares 20+ Year U.S. Treasury Bond Index ETF: https://www.blackrock.com/ca/investors/en/products/330866/ishares-20%20-year-u-s-treasury-bond-index-etf
+# 1) iShares Core Canadian 15+ Year Federal Bond Index ETF: https://www.blackrock.com/ca/investors/en/products/330862/ishares-core-canadian-15%20-year-federal-bond-index-etf
+
+# Short-Medium Maturity
+# 1) iShares Floating Rate Index ETF: https://www.blackrock.com/ca/investors/en/products/239487/ishares-floating-rate-index-etf
+# 2) iShares Core Canadian Short Term Bond Index ETF: https://www.blackrock.com/ca/investors/en/products/239491/ishares-canadian-short-term-bond-index-etf
+
+# Cash or cash equivalents <- this will be my flight to quality piece if required
+# 1) Scotiabank ISA currently paying 2.2% and its CIDC: https://ads.scotiabank.com/investment-savings-account
+        # Or equivalent bank ISA or something
+# For Analysis in portfolio use Cash.TO: money market ticker 2.2% yield
+# Given rising rates environment, and inflatation persisting, unlikely for rate cuts (likely hikes) into 2027
+
+# Equities:
+
+# US (tsx CAD):
+# 1) iShares Core S&P U.S. Total Market Index ETF (3500 stocks - more dilute s&p): https://www.blackrock.com/ca/investors/en/products/272104/ishares-core-sp-us-total-market-index-etf
+# US (USD - requires norberts gambit)
+# 2) VTV Vanguard Value ETF: https://investor.vanguard.com/investment-products/etfs/profile/vtv
+#       Differerent large cap value: Schwab U.S. Dividend Equity ETF https://www.schwabassetmanagement.com/products/schd
+# 3) Avantis U.S. Small Cap Value ETF AVUV: https://www.avantisinvestors.com/avantis-investments/avantis-us-small-cap-value-etf/
+
+# Canadian
+# 1) iShares Core S&P/TSX Capped Composite Index ETF: https://www.blackrock.com/ca/investors/en/products/239837/ishares-sptsx-capped-composite-index-etf
+
+# International developed
+# 1) (VIU) Vanguard FTSE Developed All Cap ex North America Index ETF: https://www.vanguard.ca/en/product/etf/equity/9569/vanguard-ftse-developed-all-cap-ex-north-america-index-etf
+
+# International emerging
+# 2) (VEE) Vanguard FTSE Emerging Markets All Cap Index ETF: https://www.vanguard.ca/en/product/etf/equity/9556/vanguard-ftse-emerging-markets-all-cap-index-etf
+
+
+# Real Assets
+# Current CAPREIT position in my account
+
+# Others
+# Current VOLX in my account
+
+# Investigate adding gold for diversification purpose
+# gold is here as a tail hedge against an inflationary or supply-shock-driven equity drawdown, specifically AI-capex unwind or a Taiwan disruption, sized small, not as a core holding.
+# 1) iShares Gold Bullion ETF: https://www.blackrock.com/ca/investors/en/products/272269/ishares-gold-bullion-etf
+
+
+# Michael's Portfolio
+# Primary Assumptions: Rate hikes coming 2027 (US Fed and BOC), value tilt to avoid concentration in AI/tech names
+# Main focus is protection against excess valuations (primarliy in tech)
+# Gold may be included to protect against geopolitical tensions/ai driven market drawdown
+# Fixed Income:
+# 1) iShares Floating Rate Index ETF: https://www.blackrock.com/ca/investors/en/products/239487/ishares-floating-rate-index-etf
+# 2) Scotiabank ISA currently paying 2.2% and its CIDC: https://ads.scotiabank.com/investment-savings-account
+#     For Analysis in portfolio use Cash.TO: money market ticker 2.2% yield
+# Equities:
+# 1) iShares Core S&P U.S. Total Market Index ETF (3500 stocks - more dilute s&p): https://www.blackrock.com/ca/investors/en/products/272104/ishares-core-sp-us-total-market-index-etf
+# US (USD - requires norberts gambit)
+# 2) VTV Vanguard Value ETF: https://investor.vanguard.com/investment-products/etfs/profile/vtv
+# 3) Avantis U.S. Small Cap Value ETF AVUV: https://www.avantisinvestors.com/avantis-investments/avantis-us-small-cap-value-etf/
+# Canadian
+# 4) iShares Core S&P/TSX Capped Composite Index ETF: https://www.blackrock.com/ca/investors/en/products/239837/ishares-sptsx-capped-composite-index-etf
+# International developed
+# 5) (VIU) Vanguard FTSE Developed All Cap ex North America Index ETF: https://www.vanguard.ca/en/product/etf/equity/9569/vanguard-ftse-developed-all-cap-ex-north-america-index-etf
+# International emerging
+# 6) (VEE) Vanguard FTSE Emerging Markets All Cap Index ETF: https://www.vanguard.ca/en/product/etf/equity/9556/vanguard-ftse-emerging-markets-all-cap-index-etf
+# Real Assets:
+# 1) iShares Gold Bullion ETF: https://www.blackrock.com/ca/investors/en/products/272269/ishares-gold-bullion-etf
+# Existing Positions:
+# 1) CAPREIT, small equity allocation
+# 2) VOLX, small vix allocaitonc
